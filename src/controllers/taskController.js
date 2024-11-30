61deaaaaff3618a2f1abf45130c644c7f74ba076
anamilchert <ana.milchert@hotmail.com>
2024-11-30 14:26:14 -0300

    criando a função de criar uma nova tarefa

---
+++ src/controllers/taskController.js
@@ -0,0 +1,10 @@
+let tasks = [];
+
+const createTask = (req, res) => {
+  const { title, description } = req.body;
+  const newTask = { id: tasks.length + 1, title, description };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+};
+
+module.exports = { createTask };
