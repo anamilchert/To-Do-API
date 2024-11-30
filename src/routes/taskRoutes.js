1d5058c110c8e714f9e36eae0d1768ec130c1c0d
anamilchert <ana.milchert@hotmail.com>
2024-11-30 14:32:29 -0300

    criando rota

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -4,5 +4,6 @@ const { createTask } = require("../controllers/taskController");
 const router = express.Router();
 
 router.post("/", createTask);
+router.get("/", getTasks);
 
 module.exports = router;
